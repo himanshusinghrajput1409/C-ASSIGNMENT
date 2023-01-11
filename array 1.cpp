5d6e575470551ca445b580ca7eb11498a7086645
himanshusinghrajput1409 <122454107+himanshusinghrajput1409@users.noreply.github.com>
2023-01-11 18:47:54 +0530

    Add files via upload

---
+++ array 1.cpp
@@ -0,0 +1,11 @@
+//write a program to enter a array of element and print array of 5 elements
+#include<stdio.h>
+int main()
+{int a[5],i;
+for (i=0;i<=4;i++)
+{scanf("%d",&a[i]);
+}
+for(i=0;i<=4;i++)
+{printf("%d\n",a[i]);
+}
+}
